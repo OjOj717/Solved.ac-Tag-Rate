5ca60d0df04547a00f6f482571cd6ef2eed4d737
OjOj717 <121485938+OjOj717@users.noreply.github.com>
2026-02-06 22:19:17 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -88,18 +88,7 @@ module.exports = async (req, res) => {
             <rect width="100%" height="100%" fill="#f7f8f9" rx="20"/>
             <text x="250" y="45" text-anchor="middle" fill="${tierColor}" font-family="sans-serif" font-size="20" font-weight="bold" opacity="0.9">${handle.toUpperCase()}'S RATING</text>
             
-            ${ticks.map((score, index) => {
-                const r = (score / maxValue) * radius;
-                const isLast = index === ticks.length - 1;
-                
-                return `
-                    ${isLast ? '' : `<circle cx="${centerX}" cy="${centerY}" r="${r}" fill="none" stroke="#adb5bd" stroke-width="1" stroke-dasharray="5,5" opacity="0.3" />`}
-                    
-                    <text x="${centerX}" y="${centerY - r - 5}" text-anchor="middle" fill="#ced4da" font-family="sans-serif" font-size="10" font-weight="500">${score}</text>
-                    <text x="${centerX}" y="${centerY + r + 12}" text-anchor="middle" fill="#ced4da" font-family="sans-serif" font-size="10" font-weight="500">${score}</text>
-                `;
-            }).join('')}
-
+            
             ${stats.map((_, i) => {
                 const { x, y } = getCoordinates(i, stats.length, maxValue, maxValue, centerX, centerY, radius);
                 return `<line x1="${centerX}" y1="${centerY}" x2="${x}" y2="${y}" stroke="#afb8c2" stroke-width="1" opacity="0.3" />`;
@@ -108,7 +97,7 @@ module.exports = async (req, res) => {
             <polygon points="${points}" fill="${tierColor}1A" stroke="${tierColor}" stroke-width="2.5" stroke-linejoin="round" />
 
             ${stats.map((s, i) => {
-                const labelRadius = radius + 20;
+                const labelRadius = radius + 15;
                 const angle = (Math.PI * 2 / stats.length) * i - (Math.PI / 2);
                 const x = centerX + labelRadius * Math.cos(angle);
                 const y = centerY + labelRadius * Math.sin(angle);
